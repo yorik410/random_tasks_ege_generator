0190a15b7163983339a2bfe788a513e047dd1bfd
yorik410 <m.aleksandr4100@gmail.com>
2025-06-02 16:58:16 +0400

    fix

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ kompege = KompEGE()
 
 for i in range(tasks_generate):
     subject = rnd.choice(subjects)
-    task = rnd.randint(1, tasks_count_in_subject[subject])
+    task_number = rnd.randint(1, tasks_count_in_subject[subject])
     if subject != "inf":
-        task_dict = {task: 1}
+        task_dict = {task_number: 1}  # нужно для библиотеки,чтобы сгенерировать тест из одного задания с нужным номером
         id = sdamgia.generate_test(subject, task_dict)
         if get_url:
             url = f"{sdamgia._SUBJECT_BASE_URL[subject]}/test?id={id}&nt=True&pub=False"
@@ -25,5 +25,5 @@ for i in range(tasks_generate):
         else:
             print(f"{subject}: {id}")
     elif subject == "inf":
-        url = kompege.get_random_task_by_number(task, url=get_url)
+        url = kompege.get_random_task_by_number(task_number, url=get_url)
         print(f"inf: {url}")
